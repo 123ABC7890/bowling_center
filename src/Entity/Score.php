9dbe17f624d9b8954b425e1a66e0c458300f3254
123ABC7890 <186516@novacollege.nl>
2026-04-02 13:19:12 +0200

    feat: Add Score entity and repository with migration for score table

---
+++ src/Entity/Score.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ScoreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ScoreRepository::class)]
+class Score
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'json')]
+    public array $value = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
