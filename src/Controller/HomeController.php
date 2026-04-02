f2b525da85c41dcda6685c366802f37e713821b4
123ABC7890 <186516@novacollege.nl>
2026-04-02 12:41:13 +0200

    feat: Refactor HomeController and update index template to dynamically display tariffs and packages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Package;
+use App\Repository\PackageRepository;
+use App\Repository\TariffRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +12,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(TariffRepository $tariffRepository, PackageRepository $packageRepository): Response
     {
-        return $this->render('index.html.twig');
+        return $this->render('index.html.twig', [
+            'tariffs' => $tariffRepository->findAll(),
+            'snackPackages' => $packageRepository->findBy(['type' => Package::TYPE_SNACK]),
+            'partyPackages' => $packageRepository->findBy(['type' => Package::TYPE_PARTY]),
+        ]);
     }
 }
